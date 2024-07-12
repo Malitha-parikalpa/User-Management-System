1f430973a943c55c061a76fed88844152210c95c
malitha <it22324238@my.sliit.lk>
2024-07-12 13:03:31 +0530

    create routes

--- backend/router.js
+++ backend/router.js
@@ -1,2 +1,12 @@
  const express = require('express');
+ const router = express.Router();
+ const controller = require('./controller');
+
+ //create routes
+ router.get('/users',controller.getUsers);
+ router.get('/createuser',controller.addUser);
+ router.get('/updateusers',controller.updateUser);
+ router.get('/deleteuser',controller.deleteUser);
  
+module.exports = router;
+
