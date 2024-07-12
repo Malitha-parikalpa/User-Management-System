2686264ede3da603ffcd50f5a150695ebe59d405
malitha <it22324238@my.sliit.lk>
2024-07-12 19:02:41 +0530

    name for db

--- backend/model.js
+++ backend/model.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');  //import mongoose library 
 const Schema = mongoose.Schema;
 
 const userSchema  = new Schema({
     id: Number,
     name: String,
 })
+
+const User = mongoose.model('model',userSchema);
+
+module.exports = User;
