3522095573da9bf649aed495291e1386a2ee7185
malitha <it22324238@my.sliit.lk>
2024-07-12 12:23:21 +0530

    create in crud

--- backend/controller.js
+++ backend/controller.js
@@ -1,6 +1,6 @@
 const user = require('.model');
 
-//create
+//reyreave
 const getUsers = (req,res,next) => {
     User.find()
         .then(response =>{
@@ -8,9 +8,24 @@ const getUsers = (req,res,next) => {
         })
         .catch(error => {
             res.json({ message: error})
-        })
+        });
 };
  
+//create
+const addUser = (req,res,next) => {
+    const user = new User({
+        id : req.body.id,
+        name : req.body,name,
+    });
+    user.save()
+        .then(response =>{
+            res.json({response})
+        })
+        .catch(error => {
+            res.json({ message: error})
+        });
+}
+
 
 
 exports.getUsers = getUsers;
