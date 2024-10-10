220b71719f8dec793d46a03d16c3eaadb06dd319
malitha <it22324238@my.sliit.lk>
2024-10-10 13:16:31 +0530

    correction

--- testreactapp/src/Users.js
+++ testreactapp/src/Users.js
@@ -1,20 +1,43 @@
 import { Box } from "@mui/material";
 import UserForm from "./UserForm";
 import UsersTable from "./UsersTable";
-const users = [
-    {
-        id :1,
-        name:'Maliboy',
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
-    }
-    ,
-    {
-        id :2,
-        name:'Parikalpa',
-        
-    }
-]
 const Users = ()=>{
+    const [users,setUsers] = useState([]);
+    const [submitted,setSubmitted] = useState(false);
+    
+    //load wenkot useEffert eke athule tiyna ek wenawa
+    useEffect(()=>{
+        getUsers();
+    }, []);
+
+    const getUsers =() =>{
+        Axios.get('http://localhost:3001/api/users')
+            .then(response =>{
+                setUsers(response?.data?.response);
+            })
+            .catch(error => {
+                console.error("Axios Error : ", error);
+            });
+    }
+
+    const addUser = (data) =>{
+        setSubmitted(true);
+        const payload = {
+            id: data.id,
+            name: data.name,
+        }
+        Axios.post('http://localhost:3001/api/createuser',payload)
+            .then(() =>{
+                getUsers();
+                setSubmitted(false);
+            })
+            .catch(error => {
+                console.error("Axios Error : ", error);
+            });
+    }
     return(
         <Box
             sx={{
@@ -23,7 +46,9 @@ const Users = ()=>{
                 marginTop: '100px',
             }}
         >
-            <UserForm/>
+            <UserForm
+                addUser={addUser}
+            />
             <UsersTable rows={users}/>
         </Box>
        
